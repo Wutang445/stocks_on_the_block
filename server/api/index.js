364ed21b307e9a0178c8349dd2ed087ddd3f4764
Wutang445 <austinw556@gmail.com>
2020-02-25 22:10:44 -0500

    password encryption

--- server/api/index.js
+++ server/api/index.js
@@ -3,4 +3,10 @@ const router = require("express").Router();
 router.use("/stockData", require("./stockData"));
 router.use("/users", require("./users"));
 
+router.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
 module.exports = router;
--- server/db/models/users.js
+++ server/db/models/users.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const db = require("../db");
+const crypto = require("crypto");
 
 const User = db.define("user", {
   email: {
@@ -25,3 +26,38 @@ const User = db.define("user", {
 });
 
 module.exports = User;
+
+User.prototype.correctPassword = function(userPwd) {
+  return User.encryptPassword(userPwd, this.salt()) === this.password();
+};
+
+/**
+ * classMethods
+ */
+User.generateSalt = function() {
+  return crypto.randomBytes(16).toString("base64");
+};
+
+User.encryptPassword = function(plainText, salt) {
+  return crypto
+    .createHash("RSA-SHA256")
+    .update(plainText)
+    .update(salt)
+    .digest("hex");
+};
+
+/**
+ * hooks
+ */
+const setSaltAndPassword = user => {
+  if (user.changed("password")) {
+    user.salt = User.generateSalt();
+    user.password = User.encryptPassword(user.password(), user.salt());
+  }
+};
+
+User.beforeCreate(setSaltAndPassword);
+User.beforeUpdate(setSaltAndPassword);
+User.beforeBulkCreate(users => {
+  users.forEach(setSaltAndPassword);
+});
