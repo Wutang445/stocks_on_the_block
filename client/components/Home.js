61bd9e24415e563bd2d80a0253bf2675389393d5
awu-dev <austinw556@gmail.com>
2020-03-17 16:00:00 -0400

    toggle user login greeting

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const Home = () => {
+const Home = props => {
+  console.log(props);
   return (
     <div>
-      <h1>Welcome back</h1>
+      {props.user.id ? (
+        <h1>Welcome back, {props.user.email}</h1>
+      ) : (
+        <h1>Sign up to get started</h1>
+      )}
     </div>
   );
 };
 
-export default Home;
+const mapState = state => {
+  return {
+    user: state.user
+  };
+};
+
+export default connect(mapState, null)(Home);
--- public/bundle.js
+++ public/bundle.js
@@ -122,13 +122,22 @@ var App = function App() {
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-redux */ "./node_modules/react-redux/es/index.js");
 
 
-var Home = function Home() {
-  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "Welcome back"));
+
+var Home = function Home(props) {
+  console.log(props);
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, props.user.id ? react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "Welcome back, ", props.user.email) : react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "Sign up to get started"));
 };
 
-/* harmony default export */ __webpack_exports__["default"] = (Home);
+var mapState = function mapState(state) {
+  return {
+    user: state.user
+  };
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Object(react_redux__WEBPACK_IMPORTED_MODULE_1__["connect"])(mapState, null)(Home));
 
 /***/ }),
 
