454f342be38ef89d8852dcb05a6d88eab5fe4827
awu556 <austinw556@gmail.com>
2020-07-09 19:23:17 -0400

    migration to styled components

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -2,35 +2,73 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { logout } from "../store";
+import styled, { css } from "styled-components";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+
+const NavContainer = styled.div`
+  text-align: center;
+  background: dodgerblue;
+  color: white;
+  width: 100%;
+  height: 100%;
+  padding: 50px;
+`;
+
+const NavLinks = styled.a`
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+
+  &:hover {
+    background-color: red;
+  }
+`;
 
 const Navbar = (props) => {
   const { isLoggedIn } = props;
 
   return (
-    <div className="navbar-container">
+    <NavContainer>
       <h1>Stocks on the Block</h1>
 
       {isLoggedIn ? (
-        <nav className="navbar-links">
-          <Link to="/home">Home</Link>
+        <nav>
+          <NavLinks as={Link} to="/home">
+            Home
+          </NavLinks>
 
           <a href="#" onClick={props.handleClick}>
             Logout
           </a>
 
-          <Link to="/portfolio">Portfolio</Link>
-          <Link to="/transactions">Transactions</Link>
+          <NavLinks as={Link} to="/portfolio">
+            Portfolio
+          </NavLinks>
+          <NavLinks as={Link} to="/transactions">
+            Transactions
+          </NavLinks>
         </nav>
       ) : (
-        <nav className="navbar-links">
-          <Link to="/home">Home</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">SignUp</Link>
-          <Link to="/portfolio">Portfolio</Link>
-          <Link to="/transactions">Transactions</Link>
+        <nav>
+          <NavLinks as={Link} to="/home">
+            Home
+          </NavLinks>
+          <NavLinks as={Link} to="/login">
+            Login
+          </NavLinks>
+          <NavLinks as={Link} to="/signup">
+            SignUp
+          </NavLinks>
+          <NavLinks as={Link} to="/portfolio">
+            Portfolio
+          </NavLinks>
+          <NavLinks as={Link} to="/transactions">
+            Transactions
+          </NavLinks>
         </nav>
       )}
-    </div>
+    </NavContainer>
   );
 };
 
