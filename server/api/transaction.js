9001bfa27fc6f5b2f80c707abb954f96c69ab9ac
awu556 <austinw556@gmail.com>
2020-08-16 21:46:06 -0400

    arranging buy stock logic

--- server/api/transaction.js
+++ server/api/transaction.js
@@ -1,1 +1,34 @@
 const router = require("express").Router();
+const { Order, Transaction, Stock } = require("../db/models");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const userTransactionId = await Transaction.findOne({
+      where: {
+        userId: req.user.id,
+        isTransaction: true,
+      },
+      attributes: ["id"],
+    });
+
+    res.json(userTransactionId.id);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:userId", async (req, res, next) => {
+  try {
+    const userTransaction = await Transaction.findOne({
+      include: [{ model: Stock }, { attributes: ["id"] }],
+      where: {
+        userId: req.params.userId,
+        isTransaction: true,
+      },
+    });
+
+    res.json(userTransaction);
+  } catch (error) {
+    next(error);
+  }
+});
